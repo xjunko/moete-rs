2a87dd822d1b0b122cda85993d038f18620c3394
xjunko <fireredz69420@gmail.com>
2025-09-14 20:36:52 +0800

    core: implement emote manager

---
+++ src/core/emotes.rs
@@ -0,0 +1,57 @@
+use crate::Error;
+use crate::serenity;
+use log::info;
+
+pub struct EmoteManager {
+    internal: Vec<serenity::Emoji>, // Came straight from the bot's application
+    external: Vec<serenity::Emoji>, // Whitelisted servers
+}
+
+impl EmoteManager {
+    pub fn new() -> Self {
+        EmoteManager {
+            internal: Vec::new(),
+            external: Vec::new(),
+        }
+    }
+
+    /// Fetches the bot emojis from Discord.
+    async fn fetch_bot_emojis(ctx: &serenity::Context) -> Result<Vec<serenity::Emoji>, Error> {
+        let http: &serenity::Http = &ctx.http;
+        let emojis: Vec<serenity::Emoji> = http.get_application_emojis().await?;
+        Ok(emojis)
+    }
+
+    /// Returns an iterator over all emojis the bot can access.
+    pub fn global(&self) -> impl Iterator<Item = &serenity::Emoji> {
+        self.internal.iter().chain(self.external.iter())
+    }
+
+    // Load all the emojis we can use into EmoteManager.
+    pub async fn load(&mut self, ctx: &serenity::Context) {
+        self.internal = Self::fetch_bot_emojis(ctx).await.unwrap_or_default();
+        info!("Loaded {} bot emojis", self.internal.len());
+    }
+
+    // Returns emoji if the word matches an emoji name.
+    pub fn get(&self, name: &str) -> Option<&serenity::Emoji> {
+        self.global()
+            .filter(|e| e.available)
+            .find(|e| e.name.eq_ignore_ascii_case(name))
+    }
+
+    // Builds a text and transform it with emojis we can use.
+    pub fn text(&self, text: &str) -> String {
+        let content: Vec<String> = text
+            .split(" ")
+            .map(|word| {
+                if let Some(e) = self.get(word) {
+                    e.to_string()
+                } else {
+                    word.to_string()
+                }
+            })
+            .collect();
+        content.join(" ")
+    }
+}
