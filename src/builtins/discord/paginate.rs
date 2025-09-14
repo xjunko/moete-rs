213c0050daf0d1fca778179c0ca71162a0aa50ee
xjunko <fireredz69420@gmail.com>
2025-09-14 16:51:40 +0800

    paginate: add embed support

--- src/builtins/discord/paginate.rs
+++ src/builtins/discord/paginate.rs
@@ -27,8 +27,8 @@ pub async fn paginate(ctx: crate::Context<'_>, pages: &[&str]) -> Result<(), ser
         // We defined our button IDs to start with `ctx_id`. If they don't, some other command's
         // button was pressed
         .filter(move |press| press.data.custom_id.starts_with(&ctx_id.to_string()))
-        // Timeout when no navigation button has been pressed for 2 minutes
-        .timeout(std::time::Duration::from_secs(60 * 2))
+        // Timeout when no navigation button has been pressed for 30 seconds
+        .timeout(std::time::Duration::from_secs(30))
         .await
     {
         // Depending on which button was pressed, go to next or previous page
@@ -50,7 +50,67 @@ pub async fn paginate(ctx: crate::Context<'_>, pages: &[&str]) -> Result<(), ser
                 ctx.serenity_context(),
                 serenity::CreateInteractionResponse::UpdateMessage(
                     serenity::CreateInteractionResponseMessage::new()
-                        .embed(serenity::CreateEmbed::new().description(pages[current_page])),
+                        .embed(embed::create_embed().description(pages[current_page])),
+                ),
+            )
+            .await?;
+    }
+
+    Ok(())
+}
+
+/// Sends a paginated message from pages passed.
+pub async fn paginate_embed(
+    ctx: crate::Context<'_>,
+    pages: Vec<serenity::CreateEmbed>,
+) -> Result<(), serenity::Error> {
+    let ctx_id = ctx.id();
+    let prev_button_id = format!("{}prev", ctx_id);
+    let next_button_id = format!("{}next", ctx_id);
+
+    let reply = {
+        let components = serenity::CreateActionRow::Buttons(vec![
+            serenity::CreateButton::new(&prev_button_id).emoji('◀'),
+            serenity::CreateButton::new(&next_button_id).emoji('▶'),
+        ]);
+
+        poise::CreateReply::default()
+            .embed(pages[0].clone())
+            .components(vec![components])
+    };
+
+    ctx.send(reply).await?;
+
+    // Loop through incoming interactions with the navigation buttons
+    let mut current_page = 0;
+    while let Some(press) = serenity::collector::ComponentInteractionCollector::new(ctx)
+        // We defined our button IDs to start with `ctx_id`. If they don't, some other command's
+        // button was pressed
+        .filter(move |press| press.data.custom_id.starts_with(&ctx_id.to_string()))
+        // Timeout when no navigation button has been pressed for 30 seconds
+        .timeout(std::time::Duration::from_secs(30))
+        .await
+    {
+        // Depending on which button was pressed, go to next or previous page
+        if press.data.custom_id == next_button_id {
+            current_page += 1;
+            if current_page >= pages.len() {
+                current_page = 0;
+            }
+        } else if press.data.custom_id == prev_button_id {
+            current_page = current_page.checked_sub(1).unwrap_or(pages.len() - 1);
+        } else {
+            // This is an unrelated button interaction
+            continue;
+        }
+
+        // Update the message with the new page contents
+        press
+            .create_response(
+                ctx.serenity_context(),
+                serenity::CreateInteractionResponse::UpdateMessage(
+                    serenity::CreateInteractionResponseMessage::new()
+                        .embed(pages[current_page].clone()),
                 ),
             )
             .await?;
