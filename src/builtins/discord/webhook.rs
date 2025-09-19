90a2d7c4bfbf691d98288a6216d1e5ddfcd16671
xjunko <fireredz69420@gmail.com>
2025-09-20 00:10:47 +0800

    webhook: fix NoTokenSet error
    turns out bot cant use webhook created by other bots

--- src/builtins/discord/webhook.rs
+++ src/builtins/discord/webhook.rs
@@ -11,9 +11,13 @@ pub async fn get_or_create_webhook(
         Ok(hooks) => {
             // Look for an existing webhook named "Moete" and of type Incoming
             if let Some(existing) = hooks.into_iter().find(|hook| {
-                hook.kind == WebhookType::Incoming && hook.name.as_deref() == Some("Moete")
+                hook.kind == WebhookType::Incoming
+                    && hook.name.as_deref() == Some("Moete")
+                    && hook.user.as_ref().unwrap().id == ctx.cache.current_user().id
             }) {
-                Some(existing)
+                // FIXME: do we really need to do this?
+                let webhook_with_token = ctx.http.get_webhook(existing.id).await.ok()?;
+                Some(webhook_with_token)
             } else {
                 // no matching webhook found, try to create one
                 channel_id
